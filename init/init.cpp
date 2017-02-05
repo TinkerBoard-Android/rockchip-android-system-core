90906f7f79600b400fc728f152b7aea9bcb60042
Huang, Tao <huangtao@rock-chips.com>
2017-02-05 16:06:01 +0800

    Revert "init: remove support for disabled SELinux"
    This reverts commit d34e407aeb5898f19d4f042b7558420bbb3a1817.
    We need disable SELinux feature to support kernel 3.0.
    
    Change-Id: Ie3f04c90ca40c29fd7d636dde00b03b890cf4d60
    Signed-off-by: Huang, Tao <huangtao@rock-chips.com>

--- init/init.cpp
+++ init/init.cpp
@@ -607,7 +607,7 @@ static void selinux_init_all_handles(void)
     sehandle_prop = selinux_android_prop_context_handle();
 }
 
-enum selinux_enforcing_status { SELINUX_PERMISSIVE, SELINUX_ENFORCING };
+enum selinux_enforcing_status { SELINUX_DISABLED, SELINUX_PERMISSIVE, SELINUX_ENFORCING };
 
 static selinux_enforcing_status selinux_status_from_cmdline() {
     selinux_enforcing_status status = SELINUX_ENFORCING;
@@ -616,14 +616,32 @@ static selinux_enforcing_status selinux_status_from_cmdline() {
         if (key == "androidboot.selinux" && value == "permissive") {
             status = SELINUX_PERMISSIVE;
         }
+        if (key == "androidboot.selinux" && value == "disabled") {
+            status = SELINUX_DISABLED;
+        }
     });
 
     return status;
 }
 
+
+static bool selinux_is_disabled(void)
+{
+    if (ALLOW_DISABLE_SELINUX) {
+        if (access("/sys/fs/selinux", F_OK) != 0) {
+            // SELinux is not compiled into the kernel, or has been disabled
+            // via the kernel command line "selinux=0".
+            return true;
+        }
+        return selinux_status_from_cmdline() == SELINUX_DISABLED;
+    }
+
+    return false;
+}
+
 static bool selinux_is_enforcing(void)
 {
-    if (ALLOW_PERMISSIVE_SELINUX) {
+    if (ALLOW_DISABLE_SELINUX) {
         return selinux_status_from_cmdline() == SELINUX_ENFORCING;
     }
     return true;
@@ -631,6 +649,10 @@ static bool selinux_is_enforcing(void)
 
 int selinux_reload_policy(void)
 {
+    if (selinux_is_disabled()) {
+        return -1;
+    }
+
     INFO("SELinux: Attempting to reload policy files\n");
 
     if (selinux_android_reload_policy() == -1) {
@@ -670,6 +692,10 @@ static void selinux_initialize(bool in_kernel_domain) {
     cb.func_audit = audit_callback;
     selinux_set_callback(SELINUX_CB_AUDIT, cb);
 
+    if (selinux_is_disabled()) {
+        return;
+    }
+
     if (in_kernel_domain) {
         INFO("Loading SELinux policy...\n");
         if (selinux_android_load_policy() < 0) {
@@ -677,15 +703,8 @@ static void selinux_initialize(bool in_kernel_domain) {
             security_failure();
         }
 
-        bool kernel_enforcing = (security_getenforce() == 1);
         bool is_enforcing = selinux_is_enforcing();
-        if (kernel_enforcing != is_enforcing) {
-            if (security_setenforce(is_enforcing)) {
-                ERROR("security_setenforce(%s) failed: %s\n",
-                      is_enforcing ? "true" : "false", strerror(errno));
-                security_failure();
-            }
-        }
+        security_setenforce(is_enforcing);
 
         if (write_file("/sys/fs/selinux/checkreqprot", "0") == -1) {
             security_failure();
