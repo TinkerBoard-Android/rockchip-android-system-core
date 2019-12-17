3ced13cba45410b6861c0a1511f67c3944fd9ef0
Jianhui Wang <wjh@rock-chips.com>
2019-12-17 16:28:10 +0800

    ion: update iontest
    Change-Id: Id25dde1a004f9501044852f57e0a10c119de041b
    Signed-off-by: Jianhui Wang <wjh@rock-chips.com>
    
    Former-commit-id: 282e187093e690f6f8cc110df8cab42124d06ad7

--- libion/ion_test.c
+++ libion/ion_test.c
@@ -34,7 +34,7 @@ size_t len = 1024*1024, align = 0;
 int prot = PROT_READ | PROT_WRITE;
 int map_flags = MAP_SHARED;
 int alloc_flags = 0;
-int heap_mask = 1;
+int heap_mask = 2;
 int test = -1;
 size_t stride;
 
@@ -46,7 +46,7 @@ int _ion_alloc_test(int *fd, ion_user_handle_t *handle)
     if (*fd < 0)
         return *fd;
 
-    ret = ion_alloc(*fd, len, align, heap_mask, alloc_flags, handle);
+    ret = ion_alloc_fd(*fd, len, align, heap_mask, alloc_flags, handle);
 
     if (ret)
         printf("%s failed: %s\n", __func__, strerror(ret));
@@ -61,10 +61,12 @@ void ion_alloc_test()
     if(_ion_alloc_test(&fd, &handle))
         return;
 
-    ret = ion_free(fd, handle);
-    if (ret) {
-        printf("%s failed: %s %d\n", __func__, strerror(ret), handle);
-        return;
+    if (ion_is_legacy(fd)) {
+        ret = ion_free(fd, handle);
+        if (ret) {
+            printf("%s failed: %s %d\n", __func__, strerror(ret), handle);
+            return;
+        }
     }
     ion_close(fd);
     printf("ion alloc test: passed\n");
